a9671f4e5da4264f048686f5112d6b80551f8c69
Scott Wattenbarger <scottwattenbarger@gmail.com>
2023-10-04 20:17:29 -0700

    fixes contact

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -3,8 +3,8 @@ import styles from '../styles/ContactCode.module.css';
 const contactItems = [
   {
     social: 'website',
-    link: 'ScottWattenbarger.me',
-    href: 'https://.me',
+    link: 'ScottWattenbarger.com',
+    href: 'https://www.scottwattenbarger.com',
   },
   {
     social: 'email',
